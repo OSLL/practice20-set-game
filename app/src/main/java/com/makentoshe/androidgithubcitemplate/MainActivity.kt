334988b348bfeb0cadbdc5ebf04fb19392cad21d
Mironov A <Licomail256@gmail.com>
2020-06-04 00:48:52 +0300

    Add fields for 2 and 4 players

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.player_vs_ai)
+        setContentView(R.layout.set_game_4_players)
     }
 }
 
