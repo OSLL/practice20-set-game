1140aee49f6047f5842c08e136634242926398e2
Akim <akimkomir@mail.ru>
2020-06-06 18:13:34 +0300

    Added classes

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -21,10 +21,44 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent4p)
         }
 
+
         buttonAI.setOnClickListener {
             val intentAI = Intent(this, PvsAIActivity::class.java)
             startActivity(intentAI)
         }
 
+    }
+}
+
+/*
+card: color shape quantity filling isChosen
+field: cardInDeck[] cardOnField[] player[]
+player: color score playerStatus(active, passive, locked) timeLeft cardInHand[] isCorrect difficulty(time, %of mistake)
+*/
+
+public class Card {
+    var color: String = "red, green, purple"
+    var shape: String = "rhombus, oval, wave"
+    var filling: String = "full, stripped, empty"
+    var quantity: Int = -1 // 1, 2 or 3
+    var isChosen: Boolean = false
+}
+
+public class Field {
+    var cardInDeck: Array<Card> = arrayOf()
+    var cardOnField: Array<Card> = arrayOf()
+    var player: Array<Player> = arrayOf()
+}
+
+public class Player {
+    var color: String = "red, green, blue, yellow"
+    var playerStatus: String = "active, passive, locked"
+    var score: Int = -1
+    var timeLeft: Float = -1f
+    var cardInHand: Array<Card> = arrayOf()
+    var isCorrect: Boolean = false
+    public class difficulty {
+        var timeToFind: Float = -1f
+        var chanceOfMistake: Float = -1f
     }
 }
